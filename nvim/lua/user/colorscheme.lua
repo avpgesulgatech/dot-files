e754a2d3676c7ce27a8eb8b106ddf78d4f175ad2
avptech <avpgesulgatech@gmail.com>
2022-07-05 05:01:28 +0800

    Add tokyonight color scheme configuration
    Signed-off-by: avptech <avpgesulgatech@gmail.com>

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,4 +1,11 @@
-local colorscheme = "default"
+local colorscheme = "tokyonight"
+
+-- Configuration --
+if colorscheme == "tokyonight" then
+  vim.g.tokyonight_style = "night"
+  vim.g.tokyonight_transparent = "true"
+  vim.g.tokyonight_transparent_sidebar = "true"
+end
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -41,10 +41,10 @@ packer.init {
 -- Install your plugins here
 return packer.startup(function(use)
   -- My plugins here
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-
+  use "wbthomason/packer.nvim"  -- Have packer manage itself
+  use "nvim-lua/popup.nvim"     -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim"   -- Useful lua functions used ny lots of plugins
+  use "folke/tokyonight.nvim"   -- Tokyonight colorscheme
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
