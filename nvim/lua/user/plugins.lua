4b5e86ac60a57775d63b0fb78adb4ab7f478830b
avptech <avpgesulgatech@gmail.com>
2022-07-06 05:47:35 +0800

    Remove post spaces
    Signed-off-by: avptech <avpgesulgatech@gmail.com>

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -44,7 +44,7 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim"          -- Have packer manage itself
   use "nvim-lua/popup.nvim"             -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim"           -- Useful lua functions used ny lots of plugins
- 
+  
   -- Colorschemes
   use "folke/tokyonight.nvim"           -- Tokyonight colorscheme
   
@@ -53,15 +53,14 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-buffer"              -- Buffer completions
   use "hrsh7th/cmp-path"                -- Path completions
   use "hrsh7th/cmp-cmdline"             -- Cmdline completions
-  use "hrsh7th/cmp-nvim-lsp"            
+  use "hrsh7th/cmp-nvim-lsp"
   use "hrsh7th/cmp-nvim-lua"
   use "saadparwaiz1/cmp_luasnip"        -- Snipper completions
   
-
   -- Snippets
   use "L3MON4D3/LuaSnip"                -- Snippet engine
   use "rafamadriz/friendly-snippets"    -- A bunch of snippest to use
-
+  
   -- LSP
   use "neovim/nvim-lspconfig"           -- Enable LSP
   use "williamboman/nvim-lsp-installer" -- Simple to use language server installer
