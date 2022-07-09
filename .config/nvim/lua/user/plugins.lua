de5fec2973fe7a474c411ea9304d2d210910f92e
avptech <avpgesulgatech@gmail.com>
2022-07-09 23:28:04 +0800

    Setup lualine - a status bar
    Signed-off-by: avptech <avpgesulgatech@gmail.com>

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -51,7 +51,8 @@ return packer.startup(function(use)
   use "kyazdani42/nvim-tree.lua"        -- Neovim file tree explorer
   use "windwp/nvim-autopairs"           -- Autopairs  
   use "numToStr/Comment.nvim"           -- Easy comments
-  use "akinsho/bufferline.nvim"          -- Better buffer UI 
+  use "akinsho/bufferline.nvim"         -- Better buffer UI 
+  use "nvim-lualine/lualine.nvim"       -- Neovim status line
 
   -- Colorschemes
   use "folke/tokyonight.nvim"           -- Tokyonight colorscheme
